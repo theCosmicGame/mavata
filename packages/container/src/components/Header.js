c6e982016ab5f31da2f8a968a9266792973224b3
Bridget Melvin <bridgetmelvin42@gmail.com>
2022-05-13 07:44:58 -0400

    last commit before breaking navigation, changed Header.js

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            App App
           </Typography>
           <Button
             color="primary"
