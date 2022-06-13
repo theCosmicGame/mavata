17175e9122eb917b3d24c2b617bfb2c7be676f43
theCosmicGame <bridgetmelvin42@gmail.com>
2022-06-13 13:22:05 -0400

    small update to companies

---
+++ packages/companies/src/components/companies/CompaniesTable.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+
+}));
+
+const advancedData = {
+  columns: [
+    { label: 'Name', field: 'name', sort: true },
+    { label: 'Sector', field: 'sector', sort: false },
+    { label: 'Your Role', field: 'role', sort, false },
+  ],
+
+  rows: [
+    { name:  },
+  ],
+}
+
+export default function CompaniesTable() {
+  const classses = useStyles();
+  
+  return (
+
+  );
+};
